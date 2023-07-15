17c5f9a3eac448e20150b0a211120adcd8aa2c45
Andrei Scafi <andreiscafi@hotmail.com>
2023-07-15 06:13:42 -0300

    Adding Progress Bar Effect with Javascript.

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
+//Navbar Sticky Effect:
 const navbar = document.querySelector(".navbar");
-
 const navbarOffsetTop = navbar.offsetTop;
+
+//Navbar Highlight Effect:
 const sections = document.querySelectorAll('section');
 const navbarLinks = document.querySelectorAll('.navbar-link');
+
+//Progress Bars Effect:
+const progress = document.querySelector('.progress-bars-wrapper');
+const progressPercent = document.querySelectorAll('.progress-percent');
+const progressBarPercents = [97, 89, 85, 87, 80, 70, 50];
+
 
 
 
 window.addEventListener('scroll', () => {
+
+    //Navbar Sticky Effect:
     if (window.pageYOffset >= navbarOffsetTop) {
         navbar.classList.add('sticky');
     } else {
         navbar.classList.remove('sticky');
     }
 
+    //Navbar Highlight Effect:
     sections.forEach((section, i) => {
         if (window.pageYOffset >= section.offsetTop - 10) {
 
@@ -22,6 +33,15 @@ window.addEventListener('scroll', () => {
 
             navbarLinks[i].classList.add('change');
         }
-
     })
+
+    //Progress Bars Effect:
+    if (window.pageYOffset + window.innerHeight >= progress.offsetTop) {
+        progressPercent.forEach((el, i) => {
+            el.style.width = `${progressBarPercents[i]}%`;
+
+            el.previousElementSibling.firstElementChild.textContent = progressBarPercents[i];
+        })
+    }
 });
+
